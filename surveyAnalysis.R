572d163a7606c284bbbf3aa963df1eb6177cf75e
Firas Wehbe <firaswehbe@users.noreply.github.com>
2017-05-02 20:06:40 -0500

    stretched axes, color to NU purple, vertical line for deadline

--- surveyAnalysis.R
+++ surveyAnalysis.R
@@ -29,17 +29,17 @@ mysubtitle = paste("Start Date:",mystart,
 
 png(filename="output/surveyresponse.png",width=1280,height=960,res=150)
 plot(mydataframe$mytimes,mydataframe$total,
-     bty="n", main=mymaintitle, sub = mysubtitle,
-     type="s", lwd=3, col='red',
-     ylab="# of Survey Responses Completed", ylim=c(0,1200), yaxt="n", yaxs="r",
-     xlab="Time (UTC)", xlim=c(mystart,max(mytimes)+7*24*60*60), xaxt="n", xaxs="r")
+     bty="n", main=mymaintitle, col.main = '#4E2A84', sub = mysubtitle,
+     type="s", lwd=4, col='#4E2A84',
+     ylab="# of Survey Responses Completed", ylim=c(0,max(mycumulative)+500), yaxt="n", yaxs="r",
+     xlab="Time (UTC)", xlim=c(mystart,max(mytimes)+30*24*60*60), xaxt="n", xaxs="r")
 
-axis.POSIXct(1, at=seq(mystart, max(mytimes)+7*24*60*60, 7*24*60*60), format="%D")
-axis(2, seq(0,1200,100), tick=FALSE, las=2)
+axis.POSIXct(1, at=seq(mystart, max(mytimes)+30*24*60*60, 7*24*60*60), format="%D")
+axis(2, seq(0,max(mycumulative)+500,500), tick=FALSE, las=2)
 
 abline(h=seq(0,2000,500),lwd=2,lty=1)
 abline(h=seq(0,2000,100),lwd=1,lty=2)
-# abline(v=as.POSIXct("2013-09-11 21:30:00")) #When the survey went out
+abline(v=as.POSIXct("2017-05-19 23:59:59"), lwd=2, lty=1) #When accounts suspend
 # abline(v=as.POSIXct("2013-09-16 15:00:00"),lty="dashed") #When the session 001 was opened
 # abline(v=as.POSIXct("2014-06-02 13:00:00"),lty="dashed") #When the session 002 was opened
 # abline(v=as.POSIXct("2014-10-27 13:00:00"),lty="dashed") #When the session 003 was opened
