479b9a3e541fbbb0172bffb09c73eca1ecb49209
Firas Wehbe <firaswehbe@users.noreply.github.com>
2017-05-24 10:44:24 -0500

    graph forecast is shorter because we passed the deadline

--- surveyAnalysis.R
+++ surveyAnalysis.R
@@ -32,10 +32,10 @@ plot(mydataframe$mytimes,mydataframe$total,
      bty="n", main=mymaintitle, col.main = '#4E2A84', sub = mysubtitle,
      type="s", lwd=4, col='#4E2A84',
      ylab="# of Survey Responses Completed", ylim=c(0,max(mycumulative)+500), yaxt="n", yaxs="r",
-     xlab="Time (UTC)", xlim=c(mystart,max(mytimes)+30*24*60*60), xaxt="n", xaxs="r")
+     xlab="Time (UTC)", xlim=c(mystart,max(mytimes)+7*24*60*60), xaxt="n", xaxs="r")
 
-axis.POSIXct(1, at=seq(mystart, max(mytimes)+30*24*60*60, 7*24*60*60), format="%D")
-axis(2, seq(0,max(mycumulative)+500,500), tick=FALSE, las=2)
+axis.POSIXct(1, at=seq(mystart, max(mytimes)+7*24*60*60, 7*24*60*60), format="%D")
+axis(2, seq(0,max(mycumulative)+200,500), tick=FALSE, las=2)
 
 abline(h=seq(0,2000,500),lwd=2,lty=1)
 abline(h=seq(0,2000,100),lwd=1,lty=2)
